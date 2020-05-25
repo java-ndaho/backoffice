a7de1d385e14c57a233f62f4e529fb29bb81dabf
Gasana.NHonore <ndayiho5@gmail.com>
2020-05-25 15:23:08 +0200

    exercice 7 --->authentification de l'administrateur

---
+++ src/main/java/org/example/contoller/AuthenticationServlet.java
@@ -0,0 +1,30 @@
+package org.example.contoller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "AuthenticationServlet ",urlPatterns = {"/login"})
+public class AuthenticationServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        final String login = request.getParameter("login");
+        final String password = request.getParameter("password");
+        if((login.equals("michel")&&password.equals("123456")) || (login.equals("caroline")&& password.equals("abcdef"))){
+            out.println("<html><body> <p>connexion réussie</p><br>");
+            out.println("<a href=\"home\"> Allez à la home </a></body></html><br>");
+        } else {
+            out.println("<html><body> <p>login / mdp erroné</p><br>");
+            out.println("<a href=\"login.html\"> Réessayez</a></body></html><br>");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
